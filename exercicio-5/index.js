f4ea5f99c555cf76916e3c46af5bff0995968951
matheus <matzeraa16@gmai.com>
2024-05-13 15:43:43 -0300

    exercicio-5

---
+++ exercicio-5/index.js
@@ -0,0 +1,40 @@
+let array = [5,1,0,5,61,5,28,1]
+// array.reverse()
+// console.log(array)
+
+// var soma = array.reduce(function(soma, i) {
+//     return soma + i;
+// });
+// console.log(soma);
+
+// let maior = Math.max.apply(null, array)
+// console.log(maior);
+
+// for(let i = 0; i < array.length; i++){
+//     let impar = array[i]
+//     if((impar % 2) !=0){
+//         console.log(impar)
+//     }
+// }
+
+// for(let i = 0; i < array.length; i++){
+//     let par = array[i]
+//     if((par % 2) ==0){
+//         console.log(par)
+//     }
+// }
+
+// let vezes = (ar , value ) => ar.reduce((acc,item)=>value ===item ? acc + 1 : acc,0)
+// console.log(vezes(array, 1))
+
+// let numeroUnico = new Set()
+// array.forEach((numero) => {
+//     numeroUnico.add(numero);
+// });
+// console.log(numeroUnico);
+
+// let pi = array.map((x) => x * 3.14);
+
+// console.log(pi)
+
+
